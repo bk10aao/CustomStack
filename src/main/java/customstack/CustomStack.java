b91b66efbcd380e2f6ee4f145fadfbcd6a152a3b
Dr-Psycho-Ben <kane_500@hotmail.com>
2026-09-18 22:49:05 +0100

    fixing repository link

--- src/main/java/customstack/CustomStack.java
+++ src/main/java/customstack/CustomStack.java
@@ -52,7 +52,7 @@ import static java.util.Objects.requireNonNull;
  * @author Benjamin Kane
  * @see <a href="https://www.linkedin.com/in/benjamin-kane-81149482/">LinkedIn</a>
  * @see <a href="https://github.com/bk10aao">GitHub account bk10aao</a>
- * @see <a href="https://github.com/bk10aao/CustomLinkedHashMap">Repository</a>
+ * @see <a href="https://github.com/bk10aao/CustomStack>Repository</a>
  */
 public class CustomStack<E> extends AbstractList<E> implements RandomAccess, Cloneable, java.io.Serializable {
 
